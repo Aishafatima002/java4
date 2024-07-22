862ce09454d04a97f355584b7be755a249f090d5
Aisha <fatimaaisha002@gmail.com>
2024-07-22 16:06:45 +0500

    up

---
+++ script.js
@@ -0,0 +1,18 @@
+// function add(a,b) {
+//     return a+b
+// }
+// console.log(add(12,9))
+
+// function multiply(a,b){
+//     return a*Number(b)
+// }
+// console.log("2","3")
+
+//it declare 2 3
+
+// let greet="hello"
+
+//DOM:document obj material
+//it uesd html by changing js //tree line strcture
+//n css
+
